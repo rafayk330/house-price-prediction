763d57d86a0d6d8b6132ef2da18a1f01c045fd90
Rafay Khan <76584894+rafayk330@users.noreply.github.com>
2021-04-12 22:21:58 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+%matplotlib inline
